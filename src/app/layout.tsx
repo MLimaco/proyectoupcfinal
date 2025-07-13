9946e0f748bf83550ee32bc82ac9a019749d5775
MICHAEL LIMACO <limaco.michael@gmail.com>
2025-07-13 18:59:09 -0500

    endpoints mal implementados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import { Providers } from "@/views/components/shared/providers";
+import { Toaster } from "@/components/ui/use-toast";
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>{children}
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
